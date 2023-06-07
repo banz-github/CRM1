946db17b7d9935c06640b779dd0e247464dbc636
Your Name <you@example.com>
2023-06-07 20:49:47 +0800

    micro adjustments, in admin/orders can now show all infos checkpoint before fixing the errors / pending/processing/finished/rejected

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('product', 'date_created', 'status')
+    list_filter = ('status',)
+    search_fields = ('product__name', 'customer__name')  # Search by product name or customer name
+    list_per_page = 20  # Number of orders displayed per page
+
 
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Tag)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
