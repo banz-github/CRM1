b50704fb7187eec8475f7a1a1c0c94127295b384
banz-github <131415065+banz-github@users.noreply.github.com>
2023-07-26 03:21:00 +0800

    pre-oral 1.2.2 on landingpage, oksna vieworder,search,dataanalytics

--- crm1/settings.py
+++ crm1/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 #SMTP COnfiguration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
